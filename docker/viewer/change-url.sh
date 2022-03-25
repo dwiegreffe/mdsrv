e26181ddf8c803223b4fcd90c6cb27ea666b33da
Daniel Wiegreffe <daniel@informatik.uni-leipzig.de>
2022-03-25 11:04:52 +0100

    added custom url viewer

--- docker/viewer/change-url.sh
+++ docker/viewer/change-url.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "$1"
 
-sed -i "s,https://remote.sca-ds.de,$1,g" /mdsrv/build/viewer/molstar.js
+sed -i "s,https://remote.sca-ds.de,$1,g" mdsrv/build/viewer/molstar.js
+
+cd mdsrv/build/viewer && http-server -p 4242
